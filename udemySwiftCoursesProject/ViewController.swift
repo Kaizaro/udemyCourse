be7c4cbc3405fe8fd992b3de05f66fbe3388fb32
Kaizaro <kaizaro78@gmail.com>
2020-07-07 23:35:12 +0300

    Initial Commit

---
+++ udemySwiftCoursesProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udemySwiftCoursesProject
+//
+//  Created by Paul Zamshin on 07.07.2020.
+//  Copyright © 2020 Paul Zamshin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
